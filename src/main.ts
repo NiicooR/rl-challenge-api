98244cbc5ebde8b55911dd529a425f0b87dd0ddf
Nicolas Ramirez <nicolas@caramelpoint.com>
2023-03-08 03:38:31 -0300

    fix: added /health api, port config and uptated readme

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3001);
+  console.log('App listening port ', process.env.PORT);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
